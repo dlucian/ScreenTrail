66006d599caab5cfd2636c286c89f38d4dd647b0
Lucian Daniliuc <dlucian@gmail.com>
2023-05-16 17:03:51 +0300

    set debug log-level

--- log_config.py
+++ log_config.py
@@ -2,4 +2,4 @@ import logging
 
 def setup_logging():
     log_format = "%(asctime)s - %(levelname)s - %(message)s"
-    logging.basicConfig(filename='output.log', level=logging.INFO, format=log_format)
+    logging.basicConfig(filename='output.log', level=logging.DEBUG, format=log_format)
